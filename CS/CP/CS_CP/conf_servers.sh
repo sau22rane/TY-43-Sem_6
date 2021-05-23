a349696f2a7430a70718c503e7780da5438a0153
sau22rane <scrane22301@gmail.com>
2021-05-23 13:58:30 +0530

    script added

--- CS/CP/CS_CP/conf_servers.sh
+++ CS/CP/CS_CP/conf_servers.sh
@@ -1,4 +1,4 @@
-sed -i 's/127.0.0.1/localhost/g' kerberos/index.js kerberos/public/script/script.js kerberos/routes/authenticationRoutes.js kerberos/routes/ticketGenerator.js
-sed -i 's/127.0.0.1/localhost/g' fileServer1/index.js fileServer1/public/script/script.js fileServer1/public/index.html
-sed -i 's/127.0.0.1/localhost/g' fileServer2/index.js fileServer2/public/script/script.js fileServer2/public/index.html
-sed -i 's/127.0.0.1/localhost/g' fileServer3/index.js fileServer3/public/script/script.js fileServer3/public/index.html
+sed -i 's/localhost/127.0.0.1/g' kerberos/index.js kerberos/public/script/script.js kerberos/routes/authenticationRoutes.js kerberos/routes/ticketGenerator.js
+sed -i 's/localhost/127.0.0.1/g' fileServer1/index.js fileServer1/public/script/script.js fileServer1/public/index.html
+sed -i 's/localhost/127.0.0.1/g' fileServer2/index.js fileServer2/public/script/script.js fileServer2/public/index.html
+sed -i 's/localhost/127.0.0.1/g' fileServer3/index.js fileServer3/public/script/script.js fileServer3/public/index.html
